ac1983152c3744541bae7e210211053d73128720
MykalB <mykalb081@gmail.com>
2021-06-04 10:47:00 -0400

    Functionality
    - install 'marked' library
    - implement markdown parser functionality with 'marked'
    - install 'bootstrap' library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 
+let marked = require('marked');
+
 class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      text: ''
+      text: '# Markdown Previewer'
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -16,8 +18,7 @@ class App extends React.Component{
     return (
       <div className="App">
        <textarea id='editor' onChange={this.handleChange}></textarea>
-       <div id='preview'>
-         <p>{this.state.text}</p>
+       <div id='preview' dangerouslySetInnerHTML={{__html: marked(this.state.text),}}>
        </div>
       </div>
     );
