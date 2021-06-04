19750b1dcf1a66954b93bddb275e17dcc057303c
MykalB <mykalb081@gmail.com>
2021-06-04 12:08:26 -0400

    Styling
    - use CSS objects to style input, output, and title components
    - use Bootstrap for styling components
    - replace icon
    - code cleanup

--- src/App.js
+++ src/App.js
@@ -16,13 +16,45 @@ class App extends React.Component{
   render(){
     return (
       <div className="App">
-       <textarea id='editor' onChange={this.handleChange}></textarea>
-       <div id='preview' dangerouslySetInnerHTML={{__html: marked(this.state.text),}}>
-       </div>
+        <div className='container-fluid'>
+          <h1 className='text-center'>Markdown Previewer</h1>
+          <br />
+          <div className="row">
+            <div className='col-lg-6'>
+              <h2 className='text-center' style={titleStyle}>Input</h2>
+              <textarea id='editor' style={inputStyle} onChange={this.handleChange} placeholder={this.state.text}></textarea>
+            </div>
+            <div className='col-lg-6'>
+              <h2 className='text-center' style={titleStyle}>Preview</h2>
+              <div id='preview' style={outputStyle} className='border bg-light' dangerouslySetInnerHTML={{__html: marked(this.state.text),}} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
   
+}
+
+let inputStyle = {
+  minWidth: '100%',
+  height: '70vh',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  padding: 10
+};
+
+let outputStyle = {
+  midWidth: '100%',
+  height: '70vh',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  padding: 10,
+  overflowY: 'auto'
+};
+
+let titleStyle = {
+  backgroundColor: '#18b500'
 }
 
 export default App;
